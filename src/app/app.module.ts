b2ea7db2db8cc0fee45aea6d40d9fe4b07ee2781
Emiliano <emiliano@gmail.com>
2023-03-21 16:41:55 -0300

    Primer commit de la web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ClientsComponent,
+    ContactusComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
